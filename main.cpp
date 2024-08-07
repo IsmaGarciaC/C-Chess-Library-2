1fa20a359106667808206f2c67e65bf6e4833ce8
Ismael Garcia <melgarciacas@gmail.com>
2024-08-07 11:35:14 -0500

    Version corregida usando el main.cpp recomendado funcional

--- main.cpp
+++ main.cpp
@@ -10,23 +10,21 @@ int main() {
 
     Board t1(8, "RNBQKBNRPPPPPPPP--------------------------------pppppppprnbqkbnr"); 
 
-    Piece *piece = t1.getSquares()[1].getPiece(); // Getting the piece at a specific position in this case the knight at b1
-    if (piece) {
-        num_mvs = piece->possibleMoves(mvs_1); // Getting the possible moves for the piece
-        cout << "There are " << num_mvs << " possible moves for the knight" << endl; // Printing the number of possible moves
-        cout << "On the board: RNBQKBNRPPPPPPPP--------------------------------pppppppprnbqkbnr" << endl;
-        cout << "From the current position: " << piece->getPosition() << endl; // Printing the current position of the piece
-        for (int i = 0; i < num_mvs; i++) {
-            cout << "Move[" << i << "]: " << *mvs_1[i] << endl; // Printing each possible move
-        }
-
-        for (int i = 0; i < num_mvs; i++) {
-            delete mvs_1[i]; // Delete each Coordinates object
-        }
-        delete[] mvs_1; // Delete the array of Coordinates pointers
-    } else {
-        cout << "No piece found at the specified position." << endl; // Print a message if no piece is found
+    num_mvs = t1.getSquares()[1].getPiece()->possibleMoves(mvs_1);
+    cout << "There are "<<num_mvs <<" possible moves" << endl;
+    cout << " On the board " << "RNBQKBNRPPPPPPPP--------------------------------pppppppprnbqkbnr" << endl;
+    cout << "From the current position " << t1.getSquares()[1].getCoordinates() <<endl;
+    for (int i = 0; i < num_mvs; i++)
+    {
+        cout << "Movs["<< i << "]:" <<*mvs_1[i] <<endl;
     }
+    
+    for (int i = 0; i < num_mvs; i++)
+    {
+        delete mvs_1[i];
+    }
+    free (mvs_1);
 
-    return 0; 
+    return 0;
+    
 }
